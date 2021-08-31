809fe3fd81c7e28c22fc21460be77808540cce85
kiyan rezaee <kiyanrezaee176@gmail.com>
2021-09-01 04:04:07 +0430

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function translateToPersian() {
         a.style.direction = "rtl"
         a.innerHTML = `
         امروزه، برنامه نویسی کامپیوتری به یکی از مهارتهای مورد نیاز در بسیاری از علوم تبدیل شده است. در این درس، با مفاهیم پیشرفته برنامه نویسی آشنا خواهیم شد. برنامه نویسی شیء گرا، ساختمان داده ها و طراحی و تحلیل الگوریتمهای کاربردی، از جمله مهمترین این مفاهیم هستند. اگرچه، تمامی این مفاهیم مستقل از یک زبان برنامه نویسی خاص قابل طرح می باشند، در این درس از زبان محبوب <a class="link" href="https://www.python.org/" target="_blank">پایتون</a> به عنوان ابزاری جهت پیاده سازی مفاهیم مورد نظر استفاده خواهیم کرد. دلیل این انتخاب، سادگی، محبوبیت و متن باز بودن این زبان برنامه نویسی است. در این درس، تمرکز ویژه ای بر فعالیت های عملی و گروهی خواهد بود. در طول یک ترم، دانشجویان با نحوه کدنویسی شیء گرا آشنا شده و این مفهوم را در قالب یک پروژه گروهی پایانی پیاده سازی خواهند کرد. در پایان این واحد درسی از دانشجویان محترم انتظار می رود تا بتوانند برای پروژه های بزرگ کلاس های مختلف را طراحی کرده، از ساختمان داده های مناسب برای آنها بهره برده و واسط های گرافیکی کارآمد طراحی کنند. جهت دریافت اطلاعات تکمیلی، به صفحه <a class="link"
-        href="./syllabus.html">رئوس مطالب</a> مراجعه نمائید.
+        href="./pages/syllabus.html">رئوس مطالب</a> مراجعه نمائید.
         `
         var a = document.getElementById('4')
         a.classList.add('persian')
@@ -474,7 +474,7 @@ function translateToEnglish() {
       design a graphical user
       interface for your project.
       For more information visit the <a class="link"
-        href="./syllabus.html">Syllabus</a> page.`
+        href="./pages/syllabus.html">Syllabus</a> page.`
 
         var a = document.getElementById('4')
         a.classList.remove('persian')
